24b35ae52200c3a5e87926043b19ea39c7403892
Fabricyo Barreto <bass.barreto@gmail.com>
2021-04-01 11:04:15 -0300

    Global: Foi definido estilos globais e adicionado recursos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <h1>DTmoney</h1>
+    <>
+      <h1>DTmoney</h1>
+      <GlobalStyle />
+    </>
   );
 }
 
